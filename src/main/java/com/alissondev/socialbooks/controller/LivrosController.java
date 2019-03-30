4ff55b31a846924302e7caffca6255f11b7ea0b0
Alisson Youssf <youssfbr@gmail.com>
2019-03-30 15:47:52 -0300

    Salvando no BD

--- src/main/java/com/alissondev/socialbooks/controller/LivrosController.java
+++ src/main/java/com/alissondev/socialbooks/controller/LivrosController.java
@@ -3,6 +3,7 @@ package com.alissondev.socialbooks.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +12,20 @@ import com.alissondev.socialbooks.entities.Livro;
 import com.alissondev.socialbooks.repository.LivrosRepository;
 
 @RestController
+@RequestMapping("/livros")
 public class LivrosController {
 	
 	@Autowired
 	private LivrosRepository livrosRepository;
 
-	@RequestMapping(value = "/livros", method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public List<Livro> listar() {
 		
 		return livrosRepository.findAll();
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public void salvar(@RequestBody Livro livro) {
+		livrosRepository.save(livro);
 	}
 }
