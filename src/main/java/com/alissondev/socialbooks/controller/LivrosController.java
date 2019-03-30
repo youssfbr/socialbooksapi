a9928fd996574dbe3a7609bfc871ba70d8e66186
Alisson Youssf <youssfbr@gmail.com>
2019-03-30 15:22:22 -0300

    Retornando do BD

--- src/main/java/com/alissondev/socialbooks/controller/LivrosController.java
+++ src/main/java/com/alissondev/socialbooks/controller/LivrosController.java
@@ -1,25 +1,24 @@
 package com.alissondev.socialbooks.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alissondev.socialbooks.entities.Livro;
+import com.alissondev.socialbooks.repository.LivrosRepository;
 
 @RestController
 public class LivrosController {
+	
+	@Autowired
+	private LivrosRepository livrosRepository;
 
 	@RequestMapping(value = "/livros", method = RequestMethod.GET)
 	public List<Livro> listar() {
 		
-		Livro l1 = new Livro("Rest Aplicado");
-		Livro l2 = new Livro("Java passo-a-passo");
-		
-		Livro[] livros = {l1, l2};
-		
-		return Arrays.asList(livros);
+		return livrosRepository.findAll();
 	}
 }
