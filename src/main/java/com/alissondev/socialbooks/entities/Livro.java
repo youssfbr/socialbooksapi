9e7761772fe59aa9ce07a9665065668e307cb4e9
Alisson Youssf <youssfbr@gmail.com>
2019-03-30 14:51:02 -0300

    Evitando o retorno de valores nulos

--- src/main/java/com/alissondev/socialbooks/entities/Livro.java
+++ src/main/java/com/alissondev/socialbooks/entities/Livro.java
@@ -3,20 +3,24 @@ package com.alissondev.socialbooks.entities;
 import java.util.Date;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;;
+
 public class Livro {
 
+	@JsonInclude(Include.NON_NULL)
 	private Long id;
-	
+	@JsonInclude(Include.NON_NULL)
 	private String nome;
-	
+	@JsonInclude(Include.NON_NULL)
 	private Date publicacao;
-	
+	@JsonInclude(Include.NON_NULL)
 	private String editora;
-	
+	@JsonInclude(Include.NON_NULL)
 	private String resumo;
-	
+	@JsonInclude(Include.NON_NULL)
 	private List<Comentario> comentarios;
-	
+	@JsonInclude(Include.NON_NULL)
 	private String autor;
 	
 	public Livro() {
